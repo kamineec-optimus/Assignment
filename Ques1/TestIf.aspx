18eec6986a2ce8263d76582670eeb254f203b521
kamineec-optimus <kaminee.chaudhary@optimusinfo.com>
2014-12-10 11:39:09 +0530

    Assignment1 Uploaded

---
+++ Ques1/TestIf.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#"  AutoEventWireup="true" CodeBehind="TestIf.aspx.cs" EnableViewState="true" Inherits="Ques1.TestIf"%>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:RadioButton ID="rbtnC" runat="server" Text="C#" GroupName="a" AutoPostBack="true"  />
+        <asp:RadioButton ID="rbtnVB" runat="server" Text="VB" GroupName="a" AutoPostBack="true" />
+        <asp:RadioButton ID="rbtnOther" runat="server" Text="Other" GroupName="a" AutoPostBack="true" />   <br />
+    <asp:Label ID="lbl" runat="server" Text=" "></asp:Label>
+
+    </div>
+    </form>
+</body>
+</html>
---
+++ Ques1/TestIf.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Ques1
+{
+    public partial class TestIf : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (rbtnC.Checked)
+            {
+                lbl.Text = "Good choice, C# is a fine language.";
+            }
+            else if (rbtnVB.Checked)
+            {
+                lbl.Text = "VB .NET: OOP, multithreading and more!";
+            }
+            else if (rbtnOther.Checked)
+            {
+                lbl.Text = "Well...good luck with that!";
+            }
+        }  
+       
+     }
+ 
+}
