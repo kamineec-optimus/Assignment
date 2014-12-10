f9674ef123fb0fa52c64b4502793a7e00da58c77
kamineec-optimus <kaminee.chaudhary@optimusinfo.com>
2014-12-10 13:16:46 +0530

    Assignment4

---
+++ Assignment4/Assignment4/TestString.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="TestString.aspx.cs" Inherits="Assignment4.TestString" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    
+    <form id="form1" runat="server">
+    <div>
+        <asp:Literal ID="Literal1" runat="server" Text=" Here are some basic operation on string:"></asp:Literal><br />
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+         <asp:Label ID="Label2" runat="server" Text=""></asp:Label><br />
+        <asp:Label ID="Label1" runat="server" Text=""></asp:Label><br />
+        <asp:Button ID="btnClone" runat="server" Text="Clone A String" OnClick="btnClone_Click" /><br />
+        <asp:Button ID="btnCompareTo" runat="server" Text="CompareTo" OnClick="btnCompareTo_Click" /><br />
+        <asp:Button ID="btnContains" runat="server" Text="Contains" OnClick="btnContains_Click" /><br />
+        <asp:Button ID="btnIndexOf" runat="server" Text="Find Index of any charecter" OnClick="btnIndexOf_Click" /><br />
+        <asp:Button ID="btnToLower" runat="server" Text="ToLower" OnClick="btnToLower_Click" /><br />
+        <asp:Button ID="btnToUpper" runat="server" Text="ToUpper" OnClick="btnToUpper_Click" /><br />
+        <asp:Button ID="btnInsert" runat="server" Text="Insert" OnClick="btnInsert_Click" /><br />
+        <asp:Button ID="btnLength" runat="server" Text="Length" OnClick="btnLength_Click" /><br />
+        <asp:Button ID="btnLastIndexOF" runat="server" Text="LastIndexOF" OnClick="btnLastIndexOF_Click" /><br />
+        <asp:Button ID="btnRemove" runat="server" Text="Remove" OnClick="btnRemove_Click" /><br />
+        <asp:Button ID="btnReplace" runat="server" Text="Replace" OnClick="btnReplace_Click" /><br />
+     </div>
+    </form>
+</body>
+</html>
---
+++ Assignment4/Assignment4/TestString.aspx.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Assignment4
+{
+    public partial class TestString : System.Web.UI.Page
+    {
+        string str = "Hello World";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+            Label1.Text = str;
+        }
+
+        protected void btnClone_Click(object sender, EventArgs e)
+        {
+            string str1 = string.Empty;
+            str1 = str.Clone().ToString();
+            Label1.Text = str1;
+        }
+
+        protected void btnCompareTo_Click(object sender, EventArgs e)
+        {
+          Label1.Text = Convert.ToInt32(str.CompareTo(TextBox1.Text).ToString()) == 1? "Not Equal" : "Equal";
+            
+        }
+
+        protected void btnContains_Click(object sender, EventArgs e)
+        {
+            
+            Label1.Text = (Convert.ToInt32(str.Contains(TextBox1.Text))) == 1 ? "Yes" : "No";
+        }
+
+        protected void btnIndexOf_Click(object sender, EventArgs e)
+        {
+            Label2.Text = "Find first index of any charecter in " + str;
+            Label2.Text = str.IndexOf(TextBox1.Text).ToString();
+
+        }
+
+        protected void btnToLower_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text = TextBox1.Text.ToLower();
+        }
+
+        protected void btnToUpper_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text = TextBox1.Text.ToUpper();
+
+        }
+
+        protected void btnInsert_Click(object sender, EventArgs e)
+        {
+            Label1.Text = str.Insert(0, TextBox1.Text);
+        }
+
+        protected void btnLength_Click(object sender, EventArgs e)
+        {
+            Label1.Text = TextBox1.Text.Length.ToString();
+        }
+
+        protected void btnLastIndexOF_Click(object sender, EventArgs e)
+        {
+            Label1.Text = str.LastIndexOf(TextBox1.Text).ToString();
+        }
+
+        protected void btnRemove_Click(object sender, EventArgs e)
+        {
+            Label1.Text = str.Remove(Convert.ToInt32(TextBox1.Text));
+        }
+
+        protected void btnReplace_Click(object sender, EventArgs e)
+        {
+            Label1.Text = str.Replace("l", TextBox1.Text);
+
+        }
+
+    }
+}
