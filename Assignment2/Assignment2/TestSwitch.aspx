86993824c9066f832657a81ddcd6b608ae795999
kamineec-optimus <kaminee.chaudhary@optimusinfo.com>
2014-12-10 12:01:09 +0530

    Assignment2

---
+++ Assignment2/Assignment2/TestSwitch.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="TestSwitch.aspx.cs" Inherits="Assignment2.TestSwitch" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:RadioButton ID="rbtnC" runat="server" Text="C#" GroupName="a" AutoPostBack="true"  />
+        <asp:RadioButton ID="rbtnVB" runat="server" Text="VB" GroupName="a" AutoPostBack="true" />
+        <asp:RadioButton ID="rbtnOther" runat="server" Text="Other" GroupName="a" AutoPostBack="true" />   <br />
+        <asp:Label ID="lbl" runat="server" Text=" "></asp:Label> 
+    </div>
+    </form>
+</body>
+</html>
---
+++ Assignment2/Assignment2/TestSwitch.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Assignment2
+{
+    public partial class TestSwitch : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int i = 0;
+            if (rbtnC.Checked)
+            {
+                i = 1;
+            }
+            else if(rbtnVB.Checked)
+            {
+                i = 2;
+            }
+            else if (rbtnOther.Checked)
+            {
+                i = 3;
+            }
+            switch (i)
+            {
+                case 1:
+                    lbl.Text= "Good choice, C# is a fine language.";
+                    break;
+                case 2:
+                    lbl.Text ="VB .NET: OOP, multithreading and more!";
+                    break;
+                case 3:
+                    lbl.Text = "Well...good luck with that!";
+                    break;
+
+            }
+        }
+    }
+}
